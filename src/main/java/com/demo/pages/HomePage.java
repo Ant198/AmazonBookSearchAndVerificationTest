cb5772f1afbd4938e8499e43a840701c536c4fa2
Anton <ridnovir@yahoo.com>
2025-02-24 14:55:48 +0200

    clean code2

--- src/main/java/com/demo/pages/HomePage.java
+++ src/main/java/com/demo/pages/HomePage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class HomePage extends PageTools {
-    private final By searchDropdownBox = By.xpath("//select[@id='searchDropdownBox']");
-    private final By inputField = By.xpath("//input[@id='twotabsearchtextbox']");
+    private final By searchDropdownBox = By.xpath("//select[@id=\"searchDropdownBox\"]");
+    private final By inputField = By.xpath("//input[@id=\"twotabsearchtextbox\"]");
 
     public WebElement getDropdownBox(String text) {
-        String option = String.format("//select[@id='searchDropdownBox']/option[text()='%s']", text);
+        String option = String.format("//select[@id=\"searchDropdownBox\"]/option[text()=\"%s\"]", text);
         return getWebElement(By.xpath(option));
     }
 
     public void setFilter(String text) {
-        String option = String.format("//select[@id='searchDropdownBox']/option[text()='%s']", text);
+        String option = String.format("//select[@id=\"searchDropdownBox\"]/option[text()=\"%s\"]", text);
         clickIfExist(searchDropdownBox);
         click(By.xpath(option));
     }
